b6553cb95e26248c34645163c9187370ca5622db
Shruthii <shruthii1410@gmail.com>
2021-04-19 15:51:46 +0530

    modifications

--- server/exceltojson.js
+++ server/exceltojson.js
@@ -1,14 +1,17 @@
 var XLSX = require('xlsx');
-var wb = XLSX.readFile('uploads/Copy of Answer keys.xlsx');
+module.exports= function(filename){
+var wb = XLSX.readFile("uploads/"+filename);
 const first_worksheet = wb.Sheets[wb.SheetNames[0]];
-const data1 = XLSX.utils.sheet_to_json(first_worksheet, { header: 1, raw: true, defval:null });
-//console.log(data);
-const second_worksheet = wb.Sheets[wb.SheetNames[1]];
-var range = XLSX.utils.decode_range(second_worksheet['!ref']);
-range.s.c = 2; // 0 == XLSX.utils.decode_col("A")
-range.e.c = 4; // 6 == XLSX.utils.decode_col("G")
-range.s.r = 2;
+var range1 = XLSX.utils.decode_range(first_worksheet['!ref']);
+range1.s.c = 2; 
+range1.e.c = 8; 
+range1.s.r = 1;
+var new_range1 = XLSX.utils.encode_range(range1);
+const data1 = XLSX.utils.sheet_to_json(first_worksheet, { raw: true,  range: new_range1, blankrows: false });
+return data1;
 
-var new_range = XLSX.utils.encode_range(range);
-const data2 = XLSX.utils.sheet_to_json(second_worksheet, { header: 1, raw: true, defval:null, range: new_range });
-console.log(data2);
+}
+
+
+
+
